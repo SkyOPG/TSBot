f52d36e5948948f56a85462374e3f12b0c824fa8
SkyOPG <nicknameopg1@gmail.com>
2023-06-26 13:14:18 +0100

    deploy script

--- handlers/deploy.ts
+++ handlers/deploy.ts
@@ -4,17 +4,14 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 
 const commands = [];
-// Grab all the command files from the commands directory you created earlier
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+const foldersPath: string = path.join(__dirname, 'commands');
+const commandFolders: Array<string> = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-	// Grab all the command files from the commands directory you created earlier
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	const commandsPath: string = path.join(foldersPath, folder);
+	const commandFiles: Array<string> = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
+		const filePath: string = path.join(commandsPath, file);
 		const command: any[] & Record<"data", any> & Record<"execute", any> = require(filePath);
 		if ('data' in command && 'execute' in command) {
 			commands.push(command.data.toJSON());
@@ -24,15 +21,12 @@ for (const folder of commandFolders) {
 	}
 }
 
-// Construct and prepare an instance of the REST module
 const rest = new REST().setToken(token);
 
-// and deploy your commands!
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
 			Routes.applicationCommands(clientId),
 			{ body: commands },
@@ -40,7 +34,6 @@ const rest = new REST().setToken(token);
 
 		console.log(`Successfully reloaded all application (/) commands.`);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
 		console.error(error);
 	}
 })();
